ba9c61d4f1245e31939e2f6daa0e797b998320ab
john morgangit config --global user.email jmm915@gmail.com <jmm915@gmail.com>
2022-03-08 13:38:10 -0600

    Linq

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myGames = new List<string>
+            { "Grand Theft Auto, Super Mario 3D, Halo, NBA 2K, NFL 2K1"};
+
+            var mygames =
+                myGames.OrderBy(name => name.Length).ToList();
+
+            foreach (var  gameName in myGames)
+            {
+                Console.WriteLine(gameName);
+            }
+        }
+    }
+}
